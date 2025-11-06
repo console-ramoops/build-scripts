3c8b900a374ef4a55b885f70a1a083eff77430d6
Pratik S <165717499+console-ramoops@users.noreply.github.com>
2025-11-06 16:36:10 +0530

    Update axion_16.sh

--- axion_16.sh
+++ axion_16.sh
@@ -16,7 +16,8 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 axion santoni user vanilla
 
-ax -b user 2>&1 | tee bacon_build.log || \
-  (m systemimage 2>&1 | tee system_build.log && \
-   m vendorimage 2>&1 | tee vendor_build.log && \
-   m bootimage 2>&1 | tee boot_build.log)
+m systemimage 
+
+m vendorimage 
+
+m bootimage
